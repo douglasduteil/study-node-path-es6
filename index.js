3168cbf290bd96c5dbe346992c44cdd1ec34c118
Douglas Duteil <douglasduteil@gmail.com>
2015-02-08 22:55:47 +0100

    chore: setup es6 coverage

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 //
+// Sources inspired by https://github.com/joyent/node/blob/v0.12.0/lib/path.js
 
-exports.foo = function foo(){
-  return 'bar';
-}
+export function dirname(path) {
+  let [root, dir] = splitPath(path);
 
-//
+  if (!root && !dir) {
+    // No dirname whatsoever
+    return '.';
+  }
 
-exports.bar = function bar(){
-  return 'foo';
-}
+  if (dir) {
+    // It has a dirname, strip trailing slash
+    dir = dir.substr(0, dir.length - 1);
+  }
+
+  return root + dir;
+};
+
+////
+
+// Split a filename into [root, dir, basename, ext], unix version
+// 'root' is just a slash, or nothing.
+var splitPathRe =
+    /^(\/?|)([\s\S]*?)((?:\.{1,2}|[^\/]+?|)(\.[^.\/]*|))(?:[\/]*)$/;
+function splitPath(filename) {
+  return splitPathRe.exec(filename).slice(1);
+};
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,12 +1,23 @@
 
-var expect = require('chai').expect;
+import {expect} from 'chai';
+import _path from 'path';
 
-var foo = require('../index').foo;
+import * as path from '../index';
 
-describe('foo', function(){
+describe('path', function(){
+  // Tests inspired by
+  // https://github.com/joyent/node/blob/v0.12.0/test/simple/test-path.js
 
-  it('should return "bar"', function(){
-    expect(foo()).to.be.equal('bar');
+  describe('dirname', function(){
+    it('should return the directory name of a path', function(){
+      expect(path.dirname('/a/b/')).to.be.equal('/a');
+      expect(path.dirname('/a/b')).to.be.equal('/a');
+      expect(path.dirname('/a')).to.be.equal('/');
+      expect(path.dirname('')).to.be.equal('.');
+      expect(path.dirname()).to.be.equal('.');
+      expect(path.dirname('/')).to.be.equal('/');
+      expect(path.dirname('////')).to.be.equal('/');
+    });
   });
 
 });
